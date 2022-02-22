3261717508b9875eddeb21e8bc6c073ebbc6a480
les <lcameron@gmvis.com>
2022-02-21 22:28:44 -0500

    more debug

--- script.js
+++ script.js
@@ -4,21 +4,24 @@ function compute()
     var principal = document.getElementById("principal").value;
     if (principal <= 0) {
         alert("Enter a positive number");
-        document.getElementById("principle").focus();
+        document.getElementById("principal").focus();
       }
+    var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
+    var amount = interest
+    console.log("principal "+principal)
+    console.log("interest "+interest)
+    console.log("amount "+amount)
+
     var year = new Date().getFullYear()+parseInt(years);
+
 
     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
 };
 
-function onchange()
+function updateRate()
 {
-    function updateRate() 
-    {
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
-    }
-};
-        
+var rateval = document.getElementById("rate").value;
+document.getElementById("rate_val").innerText=rateval;
+}
