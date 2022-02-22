248e27700f4f38c7cb37a922424a490e24f65dc8
Les <lcameron@gmvis.com>
2022-02-22 01:58:43 +0000

    debug

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ function compute()
 {
     p = document.getElementById("principal").value;
     var principal = document.getElementById("principal").value;
+    if (principal <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("principle").focus();
+      }
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
